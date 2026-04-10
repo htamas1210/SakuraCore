b964c2257b51416b0a72c7615a6ba409c1060258
Hatvani Tamás <hatvanitamas09@gmail.com>
2026-04-10 10:53:22 +0200

    deleted commented out old code and  todo comment

--- SakuraCore/src/Layer.cpp
+++ SakuraCore/src/Layer.cpp
@@ -9,10 +9,6 @@ Layer::Layer(const std::string &name, bool isActive) : m_LayerName(name), m_isAc
 
 void Layer::QueueTransition(std::unique_ptr<Layer> toLayer) {
     auto &layerStack = Application::Get().GetLayerStack();
-    // TODO: redo this based on the event video
-    // basically handle it like the other layer stuff
-    // put it into a vector and go over the list at the end of the application loop
-    auto &layerStack = Application::Get().m_LayerStack;
 
     LayerCommand command;
     command.action = LayerAction::Transition;
@@ -20,12 +16,5 @@ void Layer::QueueTransition(std::unique_ptr<Layer> toLayer) {
     command.newLayer = std::move(toLayer);
 
     layerStack.SubmitCommand(std::move(command));
-
-    // for (auto &layer : layerStack) {
-    //     if (layer.get() == this) {
-    //         layer = std::move(toLayer);
-    //         return;
-    //     }
-    // }
 }
 } // namespace SakuraVNE
